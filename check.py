e359b8fc726d79228b8492eddbfecef48d11aeed
blliu6 <lmd20010321@gmail.com>
2024-05-12 14:38:27 +0800

    5.9

--- check.py
+++ check.py
@@ -1,18 +1,52 @@
-from dreal import *
+import z3
+from z3 import *
 from aag import aag
 from read_aag import read_aag
+
+
+def get_var(var, pos):
+    if (pos & 1) == 0:
+        return var[pos // 2 - 1]
+    else:
+        return Not(var[pos // 2 - 1])
 
 
 def check(example: aag, k: int):  # 执行k轮
     # 第一轮
     episode = 0
-    x0 = [Variable(f'x{i + 1}', Variable.Binary) for i in range(example.n * 2)]
+    x0 = [Bool(f'x{i + 1}') for i in range(example.n)]
     # 首先非门关系
-    con = And()
-    i = 0
-    while i < example.n * 2:
-        con = And(con, x0[i] == Not(x0[i + 1]))
-        i = i + 2
+    solver = Solver()
+
+    for i, j in example.cache_list:
+        solver.add(Not(get_var(x0, i)))
+    # 处理与门关系
+    for i, j, k in example.and_gate_list:
+        solver.add(get_var(x0, i) == And(get_var(x0, j), get_var(x0, k)))
+
+    for item in example.output_list:
+        solver.push()
+        solver.add(get_var(x0, item))
+
+    var = [x0]
+    for k in range(10):
+        if solver.check() == z3.sat:
+            print(f'k:{k}->sat')
+            print(solver.model())
+            break
+        else:
+            for item in example.output_list:
+                solver.pop()
+            var.append([Bool(f'x{i + 1 + (k + 1) * example.n}') for i in range(example.n)])
+            for v, w in example.cache_list:
+                solver.add(get_var(var[-1], v) == get_var(var[-2], w))
+            for i, j, k in example.and_gate_list:
+                solver.add(get_var(var[-1], i) == And(get_var(var[-1], j), get_var(var[-1], k)))
+
+            for item in example.output_list:
+                solver.push()
+                solver.add(get_var(var[-1], item))
+            print(f'k:{k}->unsat')
 
 
 if __name__ == '__main__':
